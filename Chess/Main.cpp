74c0b833a26546b0081d22243ee669364a572b70
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2023-12-01 19:11:12 -0500

    Removed remarked code from Main.cpp
    All code that was moved to cBoard.cpp and remarked out in Main.cpp was removed from Main.cpp

--- Chess/Main.cpp
+++ Chess/Main.cpp
@@ -40,40 +40,11 @@ const std::map<int, std::string> pieceID = { {1, "bp"},
 
 int main()
 {
-    /*RenderWindow window(VideoMode(800, 800), "Chess");*/
-    //RenderWindow window;
-    
     cBoard board(800, 800);
     while (board.Update()) {
 
     }
     return 0;
-
-
-    //// Create board
-    /*RectangleShape squares[8][8];
-    Color sColors[2];
-    bool sColor = 1;
-    sColors[0].r = 118;
-    sColors[0].g = 150;
-    sColors[0].b = 86;
-    sColors[1].r = 238;
-    sColors[1].g = 238;
-    sColors[1].b = 210;
-
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            squares[i][j].setSize(Vector2f(100.f, 100.f));
-            squares[i][j].setOutlineThickness(-1.f);
-            squares[i][j].setOutlineColor(Color::Black);
-            squares[i][j].setPosition(Vector2f(squares[i][j].getSize().x * i, squares[i][j].getSize().y * j));
-            squares[i][j].setFillColor(sColors[sColor]);
-            sColor = !sColor;
-        }
-        sColor = !sColor;
-    }*/
 
     // Create Pieces
     /*Texture wpTex;
@@ -90,27 +61,6 @@ int main()
     // GAME LOOP
     //while (window.isOpen())
     //{
-    //    Event event;
-    //    while (window.pollEvent(event))
-    //    {
-    //        // CLOSE WINDOW EVENTS
-    //        if (event.type == Event::Closed)
-    //            window.close();
-    //        if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
-    //            window.close();
-    //    }
-
-    //    window.clear(Color::White);
-
-    //     //DRAW BOARD
-    //     
-    //    for (int i = 0; i < 8; i++)
-    //    {
-    //        for (int j = 0; j < 8; j++)
-    //            window.draw(squares[i][j]);
-    //    }
-
-
     //    // DRAW PIECES
     //    for (int i = 0; i < 8; i++)
     //    {
