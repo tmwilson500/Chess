54dc07335b7b910a1df88d3d58b69bf221418671
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2023-12-11 20:57:53 -0500

    Removed remarked code from Main.cpp

--- Chess/Main.cpp
+++ Chess/Main.cpp
@@ -9,19 +9,6 @@
 
 using namespace sf;
 
-
-//const std::map<std::string, std::string> pieceTheme1 = { {"bb","Textures/bb.png"},
-//                                                         {"bkg","Textures/bkg.png"},
-//                                                         {"bkn","Textures/bkn.png"},
-//                                                         {"bp","Textures/bp.png"}, 
-//                                                         {"bq","Textures/bq.png"}, 
-//                                                         {"br","Textures/br.png"}, 
-//                                                         {"wb","Textures/wb.png"}, 
-//                                                         {"wkg","Textures/wkg.png"}, 
-//                                                         {"wkn","Textures/wkn.png"}, 
-//                                                         {"wp","Textures/wp.png"}, 
-//                                                         {"wq","Textures/wq.png"}, 
-//                                                         {"wr","Textures/wr.png"}};
 const std::map<int, std::string> pieceTheme1 = { 
     {1, "Textures/bp.png"},
     {2, "Textures/br.png"},
@@ -48,34 +35,4 @@ int main()
 
     }
     return 0;
-
-    // Create Pieces
-    /*Texture wpTex;
-    Sprite wp;
-
-    if (!wpTex.loadFromFile(theme1.at("bb")))
-        throw "could not load wp.png";
-
-    wp.setTexture(wpTex);
-    wp.setScale(Vector2f(1.5f, 1.5f));*/
-
-
-    
-    // GAME LOOP
-    //while (window.isOpen())
-    //{
-    //    // DRAW PIECES
-    //    for (int i = 0; i < 8; i++)
-    //    {
-    //        for (int j = 0; j < 8; j++) {
-    //            window.draw(wp);
-    //            wp.setPosition(Vector2f(squares[i][j].getSize().x * i, squares[i][j].getSize().y * j));
-    //        }
-    //    }
-    //    window.draw(wp);
-
-    //    window.display();
-    //}
-
-    //return 0;
 }
