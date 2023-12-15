2d1c8d560243b49156bd323fe6f89da2d0581f9a
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2023-12-15 18:16:31 -0500

    Removed one cPiece member from 'move' struct
    Second cPiece member unnecessary now that each piece keeps track of its own move history

--- Chess/cPiece.cpp
+++ Chess/cPiece.cpp
@@ -9,7 +9,6 @@ using namespace sf;
 struct move
 {
 	int startI, startJ, endI, endJ;
-	cPiece& movPiece;  //The piece that was moved
 	cPiece& movPiece2; //The second piece that was moved (used only for castle moves)
 	cPiece& capPiece;  //The piece that was captured (nullptr if move did not result in capture)
 };
