4015c25ebd0888590e3250cefc8b563fd4b6cd9d
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2023-12-19 18:57:50 -0500

    Remarked 'move' struct & its use as a cPiece member (non-functional)

--- Chess/cPiece.cpp
+++ Chess/cPiece.cpp
@@ -6,19 +6,19 @@
 
 using namespace sf;
 
-struct move
-{
-	int startI, startJ, endI, endJ;
-	cPiece* movPiece2; //The second piece that was moved (used only for castle moves)
-	cPiece* capPiece;  //The piece that was captured (nullptr if move did not result in capture)
-};
+//struct move
+//{
+//	int startI, startJ, endI, endJ;
+//	cPiece* movPiece2; //The second piece that was moved (used only for castle moves)
+//	cPiece* capPiece;  //The piece that was captured (nullptr if move did not result in capture)
+//};
 
 class cPiece {
 
 public:
 	Sprite sprite;
 	Texture pTex;
-	std::vector<move> moveHist;
+	//std::vector<move> moveHist;
 	bool draw = false;
 	int ID = 0;
 	float x = 0;
