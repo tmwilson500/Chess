b11e2ee1b248e852c517e9ef104ff24203bd54ec
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2023-12-18 20:18:37 -0500

    initialized 'move' struct members 'movPiece2' & 'capPiece'  to nullptr
    members default to nullptr as they are not always used

--- Chess/cPiece.cpp
+++ Chess/cPiece.cpp
@@ -9,8 +9,8 @@ using namespace sf;
 struct move
 {
 	int startI, startJ, endI, endJ;
-	cPiece* movPiece2; //The second piece that was moved (used only for castle moves)
-	cPiece* capPiece;  //The piece that was captured (nullptr if move did not result in capture)
+	cPiece* movPiece2 = nullptr; //The second piece that was moved (used only for castle moves)
+	cPiece* capPiece = nullptr;  //The piece that was captured (nullptr if move did not result in capture)
 };
 
 class cPiece {
