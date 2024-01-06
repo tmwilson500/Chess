f6f661420d6a672f23e13768d828eb441cbff949
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2024-01-05 20:54:05 -0500

    Added Doxygen configuration file for project and generated first version of documentation

---
+++ Chess/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['cboard_0',['cBoard',['../classc_board.html',1,'']]],
+  ['cmove_1',['cMove',['../structc_move.html',1,'']]],
+  ['cpiece_2',['cPiece',['../classc_piece.html',1,'']]]
+];
