a7f637e5909e621d1a5351fcc9c6ba99c5736462
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2024-01-06 21:52:58 -0500

    Removed un-used, remarked code from cPiece.cpp

--- Chess/cPiece.cpp
+++ Chess/cPiece.cpp
@@ -1,8 +1,1 @@
 #include "cPiece.h"
-
-//struct move
-//{
-//	int startI, startJ, endI, endJ;
-//	cPiece* movPiece2; //The second piece that was moved (used only for castle moves)
-//	cPiece* capPiece;  //The piece that was captured (nullptr if move did not result in capture)
-//};
