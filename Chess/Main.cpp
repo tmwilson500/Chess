97fb945021689c7c65a55a76f142360690434855
tmwilson500 <44278526+tmwilson500@users.noreply.github.com>
2024-01-06 22:18:53 -0500

    Added comments to Main.cpp to explain code as an example
    Also updated documentation files

--- Chess/Main.cpp
+++ Chess/Main.cpp
@@ -11,6 +11,7 @@
 */
 using namespace sf;
 
+// Define a map that correlates piece IDs to the appropriate texture file paths (should be PNG files)
 const std::map<int, std::string> pieceTheme1 = { 
     {1, "Textures/bp.png"},
     {2, "Textures/br.png"},
@@ -25,6 +26,8 @@ const std::map<int, std::string> pieceTheme1 = {
     {-5, "Textures/wq.png"},
     {-6, "Textures/wkg.png"} };
 
+// Define an array of RGB color codes for dark & light squares on the board
+// Array should be formatted as: { {dark.r, dark.g, dark.b}, {light.r, light.g, light.b} }
 const int boardTheme1[2][3] = { {118,150,86},
                                 {238,238,210} };
 
@@ -32,7 +35,10 @@ const int boardTheme1[2][3] = { {118,150,86},
 
 int main()
 {
+    // Create an instance of the cBoard class using the desired initial window size (x & y), and board theme and piece theme defined above
     cBoard board(600.f, 600.f, boardTheme1, pieceTheme1);
+    
+    // Continuously run the Update() function to check for user input until the game is finished
     while (board.Update()) {
 
     }
